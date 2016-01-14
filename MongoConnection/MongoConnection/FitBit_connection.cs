ef7245c8a15fb06293f22992dbeb612e16e26786
Petra Gospodnetic <petra.gospodnetic@gmail.com>
2016-01-14 16:46:33 +0100

    Mongo connection - one insertion to FitBit database

---
+++ MongoConnection/MongoConnection/FitBit_connection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft;
+
+
+namespace MongoConnection
+{
+    class FitBitContext
+    {
+        private IMongoDatabase db;
+
+        public FitBitContext()
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase("FitBit");
+            var collection = db.GetCollection<User>("Users");
+        }
+
+        public IMongoCollection<User> Users
+        {
+            get
+            {
+                return db.GetCollection<User>("Users");
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            FitBitContext ctx = new FitBitContext();
+            User user = new User();
+            user.Name = "Petra";
+            user.LastName = "Gospodnetić";
+            user.Age = "24";
+            ctx.Users.InsertOne(user);
+        }
+    }
+
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string  { get; set; }
+        public string Age { get; set; }
+        public string Age { get; set; }
+        public string Age { get; set; }
+        public string Age { get; set; }
+    }
+}
---
+++ MongoConnection/MongoConnection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+//using MongoDB.Driver.Core;
+
+
+
+namespace MongoConnection
+{
+    class BookContext
+    {
+        private IMongoDatabase db;
+
+        public BookContext()
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase("bookStore");
+            var collection = db.GetCollection<Book>("Book");
+        }
+
+        public IMongoCollection<Book> Books
+        {
+            get
+            {
+                return db.GetCollection<Book>("Book");
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BookContext ctx = new BookContext();
+            Book book = new Book();
+            book.Title = "Petra pise po bazi";
+            ctx.Books.InsertOne(book);
+        }
+    }
+
+    public class Book
+    {
+        public ObjectId Id { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+    }
+}
